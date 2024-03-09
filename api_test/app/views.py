f2fa2cb0dc49859569c2483090c754b62d1d9d97
italo <italomauricio98@gmail.com>
2024-03-09 19:58:14 -0300

    update

--- api_test/app/views.py
+++ api_test/app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from app.models import Todo
+from app.serializers import *
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def todo_list(request):
+    if request.method == 'GET':
+        todo = Todo.objects.all()
+        serializers = TodoSerializers(todo, many=True)
+        return Response(serializers.data)
+    elif request.method == 'POST':
+        serializers = TodoSerializers(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
