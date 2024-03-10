c354d4755d940d6eee30fdd351fd011df6c93d94
italo <italomauricio98@gmail.com>
2024-03-09 21:27:30 -0300

    update

--- api_test/app/views.py
+++ api_test/app/views.py
@@ -16,3 +16,23 @@ def todo_list(request):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def todo_detail_change_delete(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializers = TodoSerializers(todo)
+        return Response(serializers.data)
+    elif request.method == 'PUT':
+        serializers = TodoSerializers(todo, data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.erros, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
