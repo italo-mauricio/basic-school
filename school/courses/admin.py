008fa4d55e43f1a163c1113571d602d1a9550db3
italo <italomauricio98@gmail.com>
2024-04-14 23:17:46 -0300

    feat: add admin models

--- school/courses/admin.py
+++ school/courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Course, Avaliable
 
-# Register your models here.
+@admin.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = {'tittle', 'url', 'created', 'updated', 'active'}
+
+@admin.register(Avaliable)
+
+class AvaliableAdmin(admin.ModelAdmin):
+    list_display = {'course', 'name', 'avaliable', 'created', 'updated', 'active'}
